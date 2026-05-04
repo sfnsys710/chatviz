204221d8cb76492d41614bfa959cba07c0823824
sfnsys710 <soufianenidam7@gmail.com>
2026-05-04 16:19:18 +0100

    clean up

--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import Login from '@/components/login'
-
-export default function LoginPage() {
-  return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
-        <Login />
-    </div> 
-  );
-}
